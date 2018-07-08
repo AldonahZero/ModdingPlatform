015daaa088f7ebbc52014833aa789befea1c0901
Seto <61304189@qq.com>
2018-07-08 14:48:06 +0800

    add interaction

---
+++ Assets/DimensionStory/Scripts/ModdingPlatform/Runtime/Co/Kaiba/Blueeyes/Dimensionstory/ModdingPlatform/Story/Component/Interaction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Co.Kaiba.Blueeyes.Dimensionstory.ModdingPlatform.Story.Component
+{
+    public class Interaction : ConditionalSetParameter
+    {
+        public AudioClip[] TouchAudioClips;
+        public float TimeThreshold;
+    }
+}
